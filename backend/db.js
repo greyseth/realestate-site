bf3d063af35b3da390afad37bcbf04fe6782e934
greyseth <65594150+greyseth@users.noreply.github.com>
2024-04-23 15:31:26 +0700

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,8 +2,8 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "",
+  user: "you should",
+  password: "kill yourself now",
   database: "realestate",
 });
 
